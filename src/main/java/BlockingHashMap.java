620612431235a5650c281fa4b4b443cf6a887a21
vladilyushchenko <vladislavilyushchenko@gmail.com>
2021-04-01 14:24:40 +0300

    remarks fixed

--- src/main/java/BlockingHashMap.java
+++ src/main/java/BlockingHashMap.java
@@ -1,31 +1,56 @@
+/**
+ * Class presenting thread-safety hash map
+ * @param <K> type of key in KEY-VALUE pair
+ * @param <V> type of value in KEY-VALUE pair
+ */
 public class BlockingHashMap<K, V> {
 
-    private static class MapEntry<K, V> {
+    private final int INIT_SIZE = 16;
+
+    public static class MapEntry<K, V> {
         protected final K key;
         protected volatile V value;
         protected final int hash;
         protected MapEntry<K, V> next;
 
-        MapEntry(int hash, K key, V value, MapEntry<K, V> next) {
+        MapEntry(int hash, K key, V value) {
             this.value = value;
             this.hash = hash;
             this.key = key;
-            this.next = next;
         }
     }
 
     private final MapEntry<K, V>[] table;
 
+    /**
+     * Class constructor specifying number of buckets to create
+     * @param initialSize a number of buckets in map
+     */
     public BlockingHashMap(int initialSize) {
         this.table = newTable(initialSize);
     }
 
-    protected MapEntry<K, V>[] newTable(int size) {
+    /**
+     * Empty class constructor
+     */
+    public BlockingHashMap() {
+        this.table = newTable(INIT_SIZE);
+    }
+
+    private MapEntry<K, V>[] newTable(int size) {
         @SuppressWarnings("unchecked")
         MapEntry<K, V>[] nt = (MapEntry<K, V>[]) new MapEntry<?, ?>[size];
         return nt;
     }
 
+    /**
+     *
+     * <p>
+     *     this method adds key-value pair in map
+     * </p>
+     * @param key a key
+     * @param value a value
+     */
     public synchronized void put(K key, V value) {
         int hash = hashCode(key);
         int index = hash & table.length - 1;
@@ -40,15 +65,20 @@ public class BlockingHashMap<K, V> {
             }
         }
         if (curr == null) {
-            table[index] = new MapEntry<>(hash, key, value, null);
+            table[index] = new MapEntry<>(hash, key, value);
         }
         else {
-            curr.next = new MapEntry<>(hash, key, value, null);
+            curr.next = new MapEntry<>(hash, key, value);
         }
 
     }
 
-    public synchronized V get(K key) {
+    /**
+     * this methods tries to find an object with a given key in map
+     * @param key key of KEY-VALUE pair
+     * @return found object, otherwise null
+     */
+    public V get(K key) {
         int hash = hashCode(key);
         int index = hash & table.length - 1;
 
@@ -61,6 +91,11 @@ public class BlockingHashMap<K, V> {
         return null;
     }
 
+    /**
+     * this method check if map contains KEY-VALUE pair with a given key
+     * @param key key of KEY-VALUE pair
+     * @return true - if map contains this key, false otherwise
+     */
     public boolean contains(K key) {
         int hash = hashCode(key);
         int index = hash & table.length - 1;
@@ -74,8 +109,15 @@ public class BlockingHashMap<K, V> {
         return false;
     }
 
-    public int hashCode(K x) {
+
+    private int hashCode(K x) {
         int h = x.hashCode();
         return (h << 7) - h + (h >>> 9) + (h >>> 17);
+    }
+
+    public MapEntry<K, V>[] getTableCopy() {
+        MapEntry<K, V>[] tableCopy = newTable(table.length);
+        System.arraycopy(table, 0, tableCopy, 0, table.length);
+        return tableCopy;
     }
 }
--- src/test/java/BlockingHashMapTest.java
+++ src/test/java/BlockingHashMapTest.java
@@ -1,42 +1,50 @@
 import org.junit.jupiter.api.Assertions;
 
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Stream;
 
 class BlockingHashMapTest {
 
     @org.junit.jupiter.api.Test
-    void testPutByTwoThreads() throws InterruptedException {
-        BlockingHashMap<Integer, Integer> innerMap = new BlockingHashMap<>(10);
+    void testPutByMultipleThreadsDoesNotLooseNodesInBucket() throws InterruptedException {
+        BlockingHashMap<Integer, Integer> innerMap = new BlockingHashMap<>(1);
         Random rnd = new Random(System.currentTimeMillis());
-        int[] keys = new int[1000];
+        int[] keys = new int[15000];
 
         Thread thr1 = new Thread( () -> {
-            for (int i = 0; i < 500; i++) {
-                keys[i] = rnd.nextInt() % 1000;
-                int value = rnd.nextInt() % 1000;
+            for (int i = 0; i < 5000; i++) {
+                keys[i] = rnd.nextInt();
+                int value = rnd.nextInt();
                 innerMap.put(keys[i], value);
             }
         });
 
         Thread thr2 = new Thread( () -> {
-            for (int i = 500; i < 1000; i++) {
-                keys[i] = rnd.nextInt() % 1000;
-                int value = rnd.nextInt() % 1000;
+            for (int i = 5000; i < 10000; i++) {
+                keys[i] = rnd.nextInt();
+                int value = rnd.nextInt();
                 innerMap.put(keys[i], value);
             }
         });
 
-        thr1.start(); thr2.start();
-        thr1.join(); thr2.join();
+        Thread thr3 = new Thread( () -> {
+            for (int i = 10000; i < 15000; i++) {
+                keys[i] = rnd.nextInt();
+                int value = rnd.nextInt();
+                innerMap.put(keys[i], value);
+            }
+        });
+
+        thr1.start(); thr2.start(); thr3.start();
+        thr1.join(); thr2.join(); thr3.join();
 
         for (int key : keys) {
-            innerMap.contains(key);
+            Assertions.assertTrue(innerMap.contains(key));
         }
     }
 
     @org.junit.jupiter.api.Test
-    void testGetReturnsCorrect() {
+    void testGetReturnsWhatWePutBySingleThread() {
         BlockingHashMap<Integer, Integer> innerMap = new BlockingHashMap<>(50);
         Random rnd = new Random(System.currentTimeMillis());
         int[] keys = new int[1000];
@@ -55,10 +63,69 @@ class BlockingHashMapTest {
     }
 
     @org.junit.jupiter.api.Test
-    void testPutSameKeys() {
+    void testMapRewritesValueInExistingKey() {
         BlockingHashMap<Integer, Integer> map = new BlockingHashMap<>(10);
         map.put(0, 1);
         map.put(0, 0);
         Assertions.assertEquals(map.get(0), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testMapHasTrueSize() throws InterruptedException {
+        BlockingHashMap<Integer, Integer> map = new BlockingHashMap<>(10);
+
+        BlockingHashMap<Integer, Integer> innerMap = new BlockingHashMap<>(1);
+        Random rnd = new Random(System.currentTimeMillis());
+        int[] keys = new int[15000];
+
+        Thread thr1 = new Thread( () -> {
+            for (int i = 0; i < 5000; i++) {
+                keys[i] = rnd.nextInt() % 5000;
+                int value = rnd.nextInt();
+                innerMap.put(keys[i], value);
+            }
+        });
+
+        Thread thr2 = new Thread( () -> {
+            for (int i = 5000; i < 10000; i++) {
+                keys[i] = rnd.nextInt() % 5000;
+                int value = rnd.nextInt();
+                innerMap.put(keys[i], value);
+            }
+        });
+
+        Thread thr3 = new Thread( () -> {
+            for (int i = 10000; i < 15000; i++) {
+                keys[i] = rnd.nextInt() % 5000;
+                int value = rnd.nextInt();
+                innerMap.put(keys[i], value);
+            }
+        });
+
+        thr1.start(); thr2.start(); thr3.start();
+        thr1.join(); thr2.join(); thr3.join();
+
+        List<Integer> list =  arrayToList(keys);
+        int keySize = list.parallelStream().distinct().toArray().length;
+
+        BlockingHashMap.MapEntry<Integer, Integer>[] table = innerMap.getTableCopy();
+
+        int mapSize = 0;
+        for (BlockingHashMap.MapEntry<Integer, Integer> entry : table) {
+            while (entry != null)  {
+                entry = entry.next;
+                mapSize++;
+            }
+        }
+
+        Assertions.assertEquals(mapSize, keySize);
+    }
+
+    private List<Integer> arrayToList(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int elem : arr) {
+            list.add(elem);
+        }
+        return list;
     }
 }
